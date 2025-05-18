fa15e1fcf94c5e4d4fa7af67745e864ddda5bdcb
Sebastian <sebastian.hernandorv@gmail.com>
2025-05-18 09:55:55 -0500

    Creacion entidad

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users') // Si no le ponemos nada, automaticamente asigna el nombre de la tabla en plural
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // El tipo de dato se asigna segun el tipo de dato para ts
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  authStrategy: string;
+}
