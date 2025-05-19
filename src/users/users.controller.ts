9a5dc806435996297da9424dde951440b00f30fe
Sebastian <sebastian.hernandorv@gmail.com>
2025-05-19 09:29:46 -0500

    httpexception added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  HttpException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -18,7 +19,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+  create(@Body() createUserDto: CreateUserDto): Promise<User | HttpException> {
     return this.usersService.create(createUserDto);
   }
 
@@ -28,7 +29,9 @@ export class UsersController {
   }
 
   @Get(':id')
-  getUser(@Param('id', ParseIntPipe) id: number): Promise<User | null> {
+  getUser(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<User | HttpException | null> {
     console.log(typeof +id); // +id lo convierte a number automaticamente o usar ParseIntPipe o @Type(()=>number) de class-transform y tener transform en true
     return this.usersService.getUser(id);
   }
@@ -44,6 +47,14 @@ export class UsersController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Patch('user/:id')
+  updateByUsername(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.updateByUsername(+id, updateUserDto);
   }
 
   @Delete(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,7 +11,15 @@ export class UsersService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  async create(user: CreateUserDto): Promise<User> {
+  async create(user: CreateUserDto): Promise<User | HttpException> {
+    // Tiene que ser await para que espere a traer el user (si existe) de lo contrarioo siempre va a entrar al if porque userFound sin await es un obj de promesa entonces es algo, seria true
+    const userFound = await this.userRepository.findOne({
+      where: { username: user.username },
+    });
+
+    if (userFound)
+      return new HttpException('User already exists', HttpStatus.CONFLICT);
+
     const newUser = this.userRepository.create(user);
     // const userCreated = await this.userRepository.save(newUser); // Asi yo ponga await va a retornar una Promise porque la funcion es async
     return this.userRepository.save(newUser); // Esto retorna algo asincrono y se puede poner aqui el async await o podria retornar la peticion a la bd y el controlador o funcion que lo use maneja lo async
@@ -21,9 +29,14 @@ export class UsersService {
     return this.userRepository.find();
   }
 
-  getUser(id: number): Promise<User | null> {
+  async getUser(id: number): Promise<User | HttpException | null> {
+    const userFound = await this.userRepository.findOne({ where: { id } });
+
+    if (!userFound)
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+
     // this.userRepository.findBy(id); // Este busca por la columna identificador
-    return this.userRepository.findOne({ where: { id } }); // Este busca por una columna
+    return userFound; // Este busca por una columna
   }
 
   getUserByUser(user: {
@@ -36,13 +49,42 @@ export class UsersService {
     });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update({ id }, updateUserDto);
+  async update(id: number, user: UpdateUserDto) {
+    const userFound = await this.userRepository.findOne({ where: { id } });
+
+    if (!userFound)
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+    // Con assign
+    const updateUser = Object.assign(userFound, user);
+    return this.userRepository.save(updateUser);
+
+    // return this.userRepository.update({ id }, user);
   }
 
-  deleteById(id: number) {
-    // Elimina por una columna en este caso por id
-    return this.userRepository.delete({ id }); // Retorna cuantas filas fueron afectadas
+  updateByUsername(id: number, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(
+      { id, username: updateUserDto.username },
+      updateUserDto,
+    );
+  }
+
+  async deleteById(id: number) {
+    // Una forma
+    // const userFound = await this.userRepository.findOne({ where: { id } });
+
+    // if (!userFound)
+    //   return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    // // Elimina por una columna en este caso por id
+    // return this.userRepository.delete({ id }); // Retorna cuantas filas fueron afectadas
+
+    // Como delete retorna las filas afectadas, lo vamos a usar
+    const result = await this.userRepository.delete({ id });
+
+    if (result.affected === 0) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return result;
   }
 
   deleteByUser(user: { id: number; username: string; password: string }) {
@@ -54,5 +96,3 @@ export class UsersService {
     });
   }
 }
-
-// update con dos campos para validar
